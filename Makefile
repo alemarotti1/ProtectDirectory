b0ac51afc16f98b8da9965245b79a80b105460df
Alexandre Marotti da Fonseca Temporal <pvp_marotti@hotmail.com.br>
2026-09-21 05:25:40 -0300

    feat: creating a minimal version that will encrypt a text

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
+libs=$(shell pkg-config --libs --cflags gpgme)
+
 build:
 	rm ./main -f
-	gcc main.c -o main
+	gcc main.c $(libs) -o main
 	echo "-------------------------------"
 	./main
+	rm ./main -f
--- main.c
+++ main.c
@@ -1,10 +1,103 @@
+#include <gpg-error.h>
 #include <gpgme.h>
 #include <stdio.h>
+#include <string.h>
+
+int check_errors(gpg_error_t /*unsigned int*/ err) {
+  if (err) {
+    printf("\n");
+    printf(gpgme_strerror(err));
+    printf("\n");
+    return err;
+  }
+  return 0;
+}
+
+static gpgme_error_t passphrase_callback(void *hook, const char *uid_hint,
+                                         const char *passphrase_info,
+                                         int prev_was_bad, int fd) {
+  const char *passphrase = (const char *)hook;
+
+  if (prev_was_bad)
+    return gpgme_error(GPG_ERR_BAD_PASSPHRASE);
+
+  const gpg_error_t error = gpgme_io_write(fd, passphrase, strlen(passphrase));
+  if (error < 0)
+    return gpgme_error_from_errno(error);
+
+  gpgme_io_write(fd, "\n", 1);
+
+  return 0;
+}
 
 int main(void) {
+  gpg_error_t /*unsigned int*/ err;
+  gpgme_ctx_t ctx;
+  gpgme_data_t plaintext;
+  gpgme_data_t encryptedText;
 
-  printf(GPGME_VERSION);
-  printf("\n");
+  const char *message = "testing 1 2 3";
+  const char *password = "test password";
+
+  // this command is needed to start the gpgme library
+  // otherwise the gpgme_new command will return
+  // "GPG_ERR_NOT_OPERATIONAL"(or 176 in int)
+  gpgme_check_version(NULL);
+
+  err = gpgme_new(&ctx);
+  if (check_errors(err)) {
+    return err;
+  }
+  printf("gpgme iniciado com sucesso.\n");
+
+  err = gpgme_set_protocol(ctx, GPGME_PROTOCOL_OPENPGP);
+  if (check_errors(err)) {
+    return err;
+  }
+  printf("protocolo setado com sucesso.\n");
+
+  gpgme_set_passphrase_cb(ctx, passphrase_callback, (void *)password);
+
+  // converts the "message" variable into the plaintext variable
+  err = gpgme_data_new_from_mem(&plaintext, message, strlen(message), 0);
+  if (check_errors(err)) {
+    return err;
+  }
+  printf("mensagem salva com sucesso");
+
+  /* creates an output data object */
+  err = gpgme_data_new(&encryptedText);
+  if (check_errors(err)) {
+    return err;
+  }
+  printf("output criado");
+
+  // This function is used to set the pinentry mode to loopback.
+  //  If this is not set, the program will return an "operation cancelled" error
+  err = gpgme_set_pinentry_mode(ctx, GPGME_PINENTRY_MODE_LOOPBACK);
+  if (check_errors(err)) {
+    return err;
+  }
+
+  // encrypts the text
+  err = gpgme_op_encrypt(ctx, NULL, GPGME_ENCRYPT_ALWAYS_TRUST, plaintext,
+                         encryptedText);
+  if (check_errors(err)) {
+    return err;
+  }
+  printf("texto criptografado");
+
+  // gets the encrypted text
+  size_t len;
+  char *encrypted = gpgme_data_release_and_get_mem(encryptedText, &len);
+
+  if (encrypted) {
+    printf("Encrypted data (%zu bytes):\n", len);
+    fwrite(encrypted, 1, len, stdout);
+    printf("\n");
+
+    gpgme_free(encrypted);
+  }
 
   return 0;
 }
